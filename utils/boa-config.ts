6bd6d694c39a04cad7cc1ca85932b9fbfb521630
Joey Takeda <joeytakeda@users.noreply.github.com>
2025-11-11 20:32:41 -0800

    Adding multiple profiles for BOA

---
+++ utils/boa-config.ts
@@ -0,0 +1,34 @@
+import { stringify } from "jsr:@std/yaml";
+// Define the data to be written to the YAML file
+const config = {
+  project: {
+    type: "book"
+  },
+  book: {
+   title: "TEI2026",
+   chapters: ['index.qmd']
+  },
+  format: {
+    pdf: {
+    }
+  }
+};
+
+
+for await (const dirEntry of Deno.readDir("./contributions")) {
+  console.log(dirEntry.name);
+  console.log(dirEntry);
+  config.book.chapters.push(`./contributions/${dirEntry.name}`);
+}
+
+
+// Convert the JavaScript object to a YAML string with 2-space indentation
+const yamlString = stringify(config, { indent: 2 });
+
+// Write the YAML string to a file
+try {
+  await Deno.writeTextFile("_quarto-boa-test.yaml", yamlString);
+  console.log("../_quarto-boa-test.yaml.");
+} catch (error) {
+  console.error("Error writing settings.yaml:", error);
+}
