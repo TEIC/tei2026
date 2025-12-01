edbe2f63929ce103547c7dc80a0a33b4d7f644d2
Joey Takeda <joeytakeda@users.noreply.github.com>
2025-12-01 10:44:21 -0800

    Updates

--- utils/boa-config.ts
+++ utils/boa-config.ts
@@ -1,34 +1,82 @@
-import { stringify } from "jsr:@std/yaml";
-// Define the data to be written to the YAML file
-const config = {
-  project: {
-    type: "book"
-  },
-  book: {
-   title: "TEI2026",
-   chapters: ['index.qmd']
-  },
-  format: {
-    "pdf":  {
-        "documentclass": "scrbook",
-        "classoption": ["twocolumn"]
+import * as path from "jsr:@std/path";
+import { expandGlob, copy, move, ensureDir } from "jsr:@std/fs";
+import { stringify, parse } from "jsr:@std/yaml";
+
+/* Input file to be transformed */
+const INPUT_FILE = `_quarto-boa.yaml`;
+const TMP_DIR = path.join(Deno.cwd(), "tmp");
+
+/* Timestamp */
+const TIMESTAMP = new Date().toLocaleString("en-US", {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+});
+
+async function refreshDir(dir) {
+  try {
+    await Deno.lstat(dir);
+    await Deno.remove(dir, { recursive: true });
+  } catch (err) {
+    if (!(err instanceof Deno.errors.NotFound)) {
+      throw err;
     }
   }
-};
-for await (const dirEntry of Deno.readDir("./contributions")) {
-  console.log(dirEntry.name);
-  console.log(dirEntry);
-  config.book.chapters.push(`./contributions/${dirEntry.name}`);
 }
 
-
-// Convert the JavaScript object to a YAML string with 2-space indentation
-const yamlString = stringify(config, { indent: 2 });
-
-// Write the YAML string to a file
-try {
-  await Deno.writeTextFile("_quarto-boa-test.yaml", yamlString);
-  console.log("../_quarto-boa-test.yaml.");
-} catch (error) {
-  console.error("Error writing settings.yaml:", error);
+async function getConfig() {
+  try {
+    const config = parse(await Deno.readTextFile(INPUT_FILE));
+    try {
+      await Deno.remove(TMP_DIR, { recursive: true });
+    } catch (err) {
+      if (!(err instanceof Deno.errors.NotFound)) {
+        throw err;
+      }
+    }
+    await Deno.mkdir(TMP_DIR);
+    await move(INPUT_FILE, path.join(TMP_DIR, INPUT_FILE));
+    return config;
+  } catch (e) {
+    console.error(`Error parsing config: ` + e);
+  }
 }
+
+async function updateConfig() {
+  const config = await getConfig();
+  config.book.chapters = (
+    await Promise.all(
+      config.book.chapters.map(async (chapter) => {
+        if (path.isGlob(chapter)) {
+          const paths = (await Array.fromAsync(expandGlob(chapter))).filter(({ name}) => name !== "*.qmd");
+          return paths.map(({ path: p }) => path.relative(Deno.cwd(), p));
+        } else {
+          return [chapter];
+        }
+      })
+    )
+  ).flat();
+  return config;
+}
+
+async function writeConfig() {
+  // // Convert the JavaScript object to a YAML string with 2-space indentation
+  const config = await updateConfig();
+  console.log(config);
+  const yamlString = stringify(config, { indent: 2 });
+  const outPath = path.join(Deno.cwd(), INPUT_FILE);
+  // Write the YAML string to a file
+  try {
+    console.log(`Writing ${outPath}`);
+    await Deno.writeTextFile(outPath, yamlString);
+    await copy(outPath, path.join(TMP_DIR, `${INPUT_FILE}.processed` ));
+  } catch (error) {
+    console.error(`Error writing ${outPath}:`, error);
+  }
+}
+
+writeConfig();
+
